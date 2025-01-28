f5a773cc5a37b6ee5006527f6a7c59999cc0501f
HassanThePassionate <maqsoodia777@gmail.com>
2025-01-28 18:43:38 +0500

    Update

--- components/hero/Icon.tsx
+++ components/hero/Icon.tsx
@@ -6,18 +6,24 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 interface Props {
   tooltip: string;
   icon: React.ReactNode;
 }
 export const Icon = ({ tooltip, icon }: Props) => {
+  const router = useRouter();
   const [tooltipVisible, setTooltipVisible] = useState(false);
   const handleMouseEnter = () => setTooltipVisible(true);
   const handleMouseLeave = () => setTooltipVisible(false);
+  const handleClick = () => {
+    if (tooltip === "Trim Video") router.push("/player");
+  };
   return (
     <TooltipProvider delayDuration={1}>
       <Tooltip open={tooltipVisible}>
         <TooltipTrigger
+          onClick={handleClick}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
           className='hover:bg-[#ddd] dark:hover:bg-gray-600 h-10 w-10 rounded-[7px] bg-[#eee] dark:bg-gray-700 flex items-center justify-center'
